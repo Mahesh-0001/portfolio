3b89984b766ce372df67aac5a14e230b9160c84c
Mahesh-0001 <163407135+Mahesh-0001@users.noreply.github.com>
2025-02-21 20:36:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add functionality for contact form or any other dynamic features in the future
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Message sent! I will get back to you soon.");
+});
